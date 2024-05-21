54b4eeccc256eaa2a7e21dfd828a801d02d9878a
Hywenklis <hywenklis@hotmail.com>
2024-05-21 14:46:43 -0300

    Configurações iniciais

---
+++ src/test/java/com/buddy/api/BuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buddy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
