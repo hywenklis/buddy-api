5fa1d04d20d7ee76661a422458c2f9251786c79d
Hywenklis <hywenklis@hotmail.com>
2024-05-21 15:31:20 -0300

    Ajustes do checkstyle

--- src/test/java/com/buddy/api/BuddyApplicationTests.java
+++ src/test/java/com/buddy/api/BuddyApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BuddyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
