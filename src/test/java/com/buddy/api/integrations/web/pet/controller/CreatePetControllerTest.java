04d79f77ca21cbf1621ee0a3b76a05e0ee45c092
Hywenklis <hywenklis@hotmail.com>
2024-06-12 12:59:31 -0300

    Adiciona teste integrados

--- src/test/java/com/buddy/api/integrations/web/pet/controller/CreatePetControllerTest.java
+++ src/test/java/com/buddy/api/integrations/web/pet/controller/CreatePetControllerTest.java
@@ -1,12 +1,16 @@
 package com.buddy.api.integrations.web.pet.controller;
 
 import static com.buddy.api.builders.pet.PetBuilder.createPetRequest;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.buddy.api.builders.pet.PetBuilder;
 import com.buddy.api.integrations.IntegrationTestAbstract;
+import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -44,6 +48,207 @@ class CreatePetControllerTest extends IntegrationTestAbstract {
                 .andExpect(jsonPath("$.errors[0].message").value("Shelter not found"))
                 .andExpect(jsonPath("$.errors[0].httpStatus").value("NOT_FOUND"))
                 .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.NOT_FOUND.value()))
+                .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return bad request if petName is not filled in")
+    void should_return_bad_request_pet_name_not_filled() throws Exception {
+        var shelter = shelterComponent.createShelterNoPets();
+
+        var request = PetBuilder.createPetRequest(
+               null,
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                Integer.valueOf(randomNumeric(1)),
+                Double.valueOf(randomNumeric(1)),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                List.of(),
+                shelter.getId()
+        );
+
+        mockMvc
+                .perform(post("/v1/pets/register")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("name"))
+                .andExpect(jsonPath("$.errors[0].message").value("Name of mandatory pet"))
+                .andExpect(jsonPath("$.errors[0].httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return bad request if specie name is not filled in")
+    void should_return_bad_request_specie_name_not_filled() throws Exception {
+        var shelter = shelterComponent.createShelterNoPets();
+
+        var request = PetBuilder.createPetRequest(
+                randomAlphabetic(10),
+                null,
+                randomAlphabetic(10),
+                Integer.valueOf(randomNumeric(1)),
+                Double.valueOf(randomNumeric(1)),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                List.of(),
+                shelter.getId()
+        );
+
+        mockMvc
+                .perform(post("/v1/pets/register")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("specie"))
+                .andExpect(jsonPath("$.errors[0].message").value("Specie of mandatory pet"))
+                .andExpect(jsonPath("$.errors[0].httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return bad request if sex name is not filled in")
+    void should_return_bad_request_sex_name_not_filled() throws Exception {
+        var shelter = shelterComponent.createShelterNoPets();
+
+        var request = PetBuilder.createPetRequest(
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                null,
+                Integer.valueOf(randomNumeric(1)),
+                Double.valueOf(randomNumeric(1)),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                List.of(),
+                shelter.getId()
+        );
+
+        mockMvc
+                .perform(post("/v1/pets/register")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("sex"))
+                .andExpect(jsonPath("$.errors[0].message").value("Sex of mandatory pet"))
+                .andExpect(jsonPath("$.errors[0].httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return bad request if age is not filled in")
+    void should_return_bad_request_age_not_filled() throws Exception {
+        var shelter = shelterComponent.createShelterNoPets();
+
+        var request = PetBuilder.createPetRequest(
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                null,
+                Double.valueOf(randomNumeric(1)),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                List.of(),
+                shelter.getId()
+        );
+
+        mockMvc
+                .perform(post("/v1/pets/register")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("age"))
+                .andExpect(jsonPath("$.errors[0].message").value("Age of mandatory pet"))
+                .andExpect(jsonPath("$.errors[0].httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return bad request if weight is not filled in")
+    void should_return_bad_request_weight_not_filled() throws Exception {
+        var shelter = shelterComponent.createShelterNoPets();
+
+        var request = PetBuilder.createPetRequest(
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                Integer.valueOf(randomNumeric(1)),
+                null,
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                List.of(),
+                shelter.getId()
+        );
+
+        mockMvc
+                .perform(post("/v1/pets/register")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("weight"))
+                .andExpect(jsonPath("$.errors[0].message").value("Weight of mandatory pet"))
+                .andExpect(jsonPath("$.errors[0].httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return bad request if description is not filled in")
+    void should_return_bad_request_description_not_filled() throws Exception {
+        var shelter = shelterComponent.createShelterNoPets();
+
+        var request = PetBuilder.createPetRequest(
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                Integer.valueOf(randomNumeric(1)),
+                Double.valueOf(randomNumeric(1)),
+                null,
+                randomAlphabetic(10),
+                List.of(),
+                shelter.getId()
+        );
+
+        mockMvc
+                .perform(post("/v1/pets/register")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("description"))
+                .andExpect(jsonPath("$.errors[0].message").value("Description of mandatory pet"))
+                .andExpect(jsonPath("$.errors[0].httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return bad request if shelterId is not filled in")
+    void should_return_bad_request_shelterId_not_filled() throws Exception {
+        var request = PetBuilder.createPetRequest(
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                Integer.valueOf(randomNumeric(1)),
+                Double.valueOf(randomNumeric(1)),
+                randomAlphabetic(10),
+                randomAlphabetic(10),
+                List.of(),
+                null
+        );
+
+        mockMvc
+                .perform(post("/v1/pets/register")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("shelterId"))
+                .andExpect(jsonPath("$.errors[0].message").value("ShelterId of mandatory pet"))
+                .andExpect(jsonPath("$.errors[0].httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errors[0].errorCode").value(HttpStatus.BAD_REQUEST.value()))
                 .andExpect(jsonPath("$.errors[0].timestamp").isNotEmpty());
     }
 }
