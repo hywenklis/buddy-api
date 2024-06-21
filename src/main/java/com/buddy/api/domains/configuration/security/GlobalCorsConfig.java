597800a8eabbbe964db6e96bf9140c5f1973dcb0
Herbert Silva <78246751+hywenklis@users.noreply.github.com>
2024-06-21 13:40:17 -0300

    Adiciona configuração do cors (#28)

---
+++ src/main/java/com/buddy/api/domains/configuration/security/GlobalCorsConfig.java
@@ -0,0 +1,25 @@
+package com.buddy.api.domains.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
