07ea1be3c8e610a57f932b6a53eb4270005bcac0
Hywenklis <hywenklis@hotmail.com>
2024-06-21 00:52:22 -0300

    Ajuste do checkstyle

--- src/main/java/com/buddy/api/domains/pet/services/impls/FindPetImpl.java
+++ src/main/java/com/buddy/api/domains/pet/services/impls/FindPetImpl.java
@@ -8,8 +8,6 @@ import com.buddy.api.domains.pet.services.FindPet;
 import com.buddy.api.domains.specifications.PetSpecifications;
 import com.buddy.api.web.pets.requests.PetSearchCriteriaRequest;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Hibernate;
 import org.springframework.data.domain.Page;
@@ -26,8 +24,10 @@ public class FindPetImpl implements FindPet {
 
     @Override
     @Transactional(readOnly = true)
-    public Page<PetSearchCriteriaDto> findPets(PetSearchCriteriaRequest searchParams, Pageable pageable) {
-        Page<PetEntity> petPage = petRepository.findAll(PetSpecifications.withParams(searchParams), pageable);
+    public Page<PetSearchCriteriaDto> findPets(PetSearchCriteriaRequest searchParams,
+                                               Pageable pageable
+    ) {
+        var petPage = petRepository.findAll(PetSpecifications.withParams(searchParams), pageable);
         petPage.forEach(this::initializeProxies);
         return petPage.map(mapper::mapParamsToDto);
     }
@@ -35,7 +35,7 @@ public class FindPetImpl implements FindPet {
     @Override
     @Transactional(readOnly = true)
     public List<PetSearchCriteriaDto> findAllPets(PetSearchCriteriaRequest searchParams) {
-        List<PetEntity> petEntities = petRepository.findAll(PetSpecifications.withParams(searchParams));
+        var petEntities = petRepository.findAll(PetSpecifications.withParams(searchParams));
         petEntities.forEach(this::initializeProxies);
         return petEntities.stream()
                 .map(mapper::mapParamsToDto)
