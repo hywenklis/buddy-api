675c9df81c3dad79929dec6fbbb3cc760e9bb955
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-11-05 01:34:02 -0300

    feat: add `AccountBlockReasonEntity`

---
+++ src/main/java/com/buddy/api/domains/account/entities/AccountBlockReasonEntity.java
@@ -0,0 +1,67 @@
+package com.buddy.api.domains.account.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "account_block_reason")
+public class AccountBlockReasonEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    @Column(name = "account_block_reason_id", nullable = false, unique = true)
+    private UUID accountBlockReasonId;
+
+
+    @ManyToOne
+    @JoinColumn(
+            name = "account_id",
+            referencedColumnName = "account_id",
+            nullable = false,
+            updatable = false
+    )
+    @ToString.Exclude
+    private AccountEntity account;
+
+    @Column(name = "reason", columnDefinition = "TEXT", nullable = false)
+    private String reason;
+
+    @Column(name = "block_date", nullable = false)
+    private LocalDateTime blockDate;
+
+    @Column(name = "reactivation_date")
+    private LocalDateTime reactivationDate;
+
+    @Column(name = "deletionDate")
+    private LocalDateTime deletionDate;
+
+    @CreationTimestamp
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name = "updated_date", nullable = false)
+    private LocalDateTime updatedDate;
+}
