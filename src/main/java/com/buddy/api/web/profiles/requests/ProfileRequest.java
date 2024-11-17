076d865457818963b1cd05fa3112a600fc5dd8a8
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-11-17 00:56:32 -0300

    feat: add `not null` validation  field `accountId` of `ProfileRequest`

--- src/main/java/com/buddy/api/web/profiles/requests/ProfileRequest.java
+++ src/main/java/com/buddy/api/web/profiles/requests/ProfileRequest.java
@@ -1,11 +1,17 @@
 package com.buddy.api.web.profiles.requests;
 
 import com.buddy.api.domains.profile.enums.ProfileTypeEnum;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
+import lombok.Builder;
 
-public record ProfileRequest(UUID accountId,
-                             String name,
-                             String description,
-                             String bio,
-                             ProfileTypeEnum profileType) {
+@Builder
+public record ProfileRequest(
+    @NotNull(message = "Profile account ID is mandatory")
+    UUID accountId,
+    String name,
+    String description,
+    String bio,
+    ProfileTypeEnum profileType
+) {
 }
--- src/test/java/com/buddy/api/integrations/web/profile/controller/CreateProfileControllerTest.java
+++ src/test/java/com/buddy/api/integrations/web/profile/controller/CreateProfileControllerTest.java
@@ -1,45 +1,72 @@
 package com.buddy.api.integrations.web.profile.controller;
 
 import static com.buddy.api.builders.account.AccountBuilder.validAccountEntity;
-import static com.buddy.api.utils.RandomStringUtils.generateRandomString;
+import static com.buddy.api.builders.profile.ProfileBuilder.profileRequest;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.buddy.api.domains.profile.enums.ProfileTypeEnum;
 import com.buddy.api.integrations.IntegrationTestAbstract;
 import com.buddy.api.web.profiles.requests.ProfileRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.ResultActions;
 
 @DisplayName("POST /v1/profiles/register")
 public class CreateProfileControllerTest extends IntegrationTestAbstract {
 
+    private static final String PROFILE_REGISTER_URL = "/v1/profiles/register";
+    private static final String ERROR_FIELD_PATH = "$.errors[0].field";
+    private static final String ERROR_MESSAGE_PATH = "$.errors[0].message";
+    private static final String ERROR_HTTP_STATUS_PATH = "$.errors[0].httpStatus";
+    private static final String ERROR_CODE_PATH = "$.errors[0].errorCode";
+    private static final String ERROR_TIMESTAMP_PATH = "$.errors[0].timestamp";
+
     @Test
     @DisplayName("Should register a new profile successfuly")
     void should_register_new_profile() throws Exception {
-        var account = accountRepository.save(validAccountEntity().build());
-        String name = generateRandomString(6);
-        String description = generateRandomString(10);
-        String bio = generateRandomString(10);
-        ProfileTypeEnum profileType = ProfileTypeEnum.ADOPTER;
+        final var account = accountRepository.save(validAccountEntity().build());
+        final var request = profileRequest()
+            .accountId(account.getAccountId())
+            .build();
 
-        var request = new ProfileRequest(
-            account.getAccountId(),
-            name,
-            description,
-            bio,
-            profileType
-        );
-
-        mockMvc
-            .perform(post("/v1/profiles/register")
-                .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+        perfomCreateProfileRequest(request)
             .andExpectAll(
                 status().isCreated(),
                 jsonPath("$.message").value("Successfully created")
             );
+    }
+
+    @Test
+    @DisplayName("Should not create profile without account id")
+    void should_not_create_profile_without_account_id() throws Exception {
+        final var request = profileRequest().accountId(null).build();
+
+        expectBadRequestFrom(perfomCreateProfileRequest(request))
+            .andExpectAll(
+                jsonPath(ERROR_FIELD_PATH).value("accountId"),
+                jsonPath(ERROR_MESSAGE_PATH).value(
+                    "Profile account ID is mandatory"
+                )
+            );
+    }
+
+    private ResultActions expectBadRequestFrom(final ResultActions result) throws Exception {
+        return result.andExpectAll(
+            status().isBadRequest(),
+            jsonPath(ERROR_HTTP_STATUS_PATH).value(HttpStatus.BAD_REQUEST.name()),
+            jsonPath(ERROR_CODE_PATH).value(HttpStatus.BAD_REQUEST.value()),
+            jsonPath(ERROR_TIMESTAMP_PATH).isNotEmpty()
+        );
+    }
+
+    private ResultActions perfomCreateProfileRequest(final ProfileRequest request)
+        throws Exception {
+        return mockMvc
+            .perform(post(PROFILE_REGISTER_URL)
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)));
     }
 }
