f3d710fe2bf174245c0b0e605504b102ca374f59
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-11-25 20:59:45 -0300

    refactor: remove unnecessary annotation and constructor

--- src/main/java/com/buddy/api/domains/valueobjects/EmailAddress.java
+++ src/main/java/com/buddy/api/domains/valueobjects/EmailAddress.java
@@ -2,17 +2,12 @@ package com.buddy.api.domains.valueobjects;
 
 import com.buddy.api.commons.exceptions.InvalidEmailAddressException;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import java.util.Locale;
 
-@Embeddable
 public record EmailAddress(
     @Column(name = "email", nullable = false, unique = true)
     String value
 ) {
-    private EmailAddress() {
-        this("");
-    }
 
     public EmailAddress {
         if (value == null || value.isBlank()) {
