0f443f8fcab82c2898edf6c4a17bfe8be1986275
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-11-26 13:03:34 -0300

    docs: update `swagger documentation`

--- src/main/java/com/buddy/api/web/profiles/controllers/CreateProfileController.java
+++ src/main/java/com/buddy/api/web/profiles/controllers/CreateProfileController.java
@@ -3,6 +3,8 @@ package com.buddy.api.web.profiles.controllers;
 import com.buddy.api.domains.profile.services.CreateProfile;
 import com.buddy.api.web.defaultresponses.CreatedSuccessResponse;
 import com.buddy.api.web.profiles.requests.ProfileRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,12 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/profiles/register")
 @RequiredArgsConstructor
+@Tag(name = "Profile", description = "Endpoint related to profile registration")
 public class CreateProfileController {
 
     private final CreateProfile createProfile;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+        summary = "Register profile",
+        description = "Register profile with their appropriate information"
+    )
     public CreatedSuccessResponse registration(@Valid @RequestBody final ProfileRequest request) {
         createProfile.create(request.toProfileDto());
         return new CreatedSuccessResponse();
--- src/main/java/com/buddy/api/web/profiles/requests/ProfileRequest.java
+++ src/main/java/com/buddy/api/web/profiles/requests/ProfileRequest.java
@@ -2,6 +2,7 @@ package com.buddy.api.web.profiles.requests;
 
 import com.buddy.api.domains.profile.dtos.ProfileDto;
 import com.buddy.api.domains.profile.enums.ProfileTypeEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
@@ -12,14 +13,32 @@ import lombok.Builder;
 
 @Builder
 public record ProfileRequest(
+    @Schema(description = "Profile account ID", example = "600c7ef2-5868-430e-8bf1-40407c635049")
     @NotNull(message = "Profile account ID is mandatory")
     UUID accountId,
+
+    @Schema(description = "Profile name", example = "Buddy")
     @NotBlank(message = "Profile name is mandatory")
     @Size(message = "Profile name must have between 3 and 100 characters", min = 3, max = 100)
     String name,
+
+    @Schema(description = "Profile description", example = "Pet lover")
     @Size(message = "Profile description must have at most 255 characters", max = 255)
     String description,
+
+    @Schema(
+        description = "Profile bio",
+        example = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Integer augue nulla, porta eget nunc finibus, lacinia sagittis turpis.
+            Suspendisse fringilla tempus blandit. Nulla ut risus nisl. Curabitur imperdiet dapibus
+            orci, in aliquam risus fringilla vitae. Phasellus iaculis ornare libero euismod feugiat
+            Quisque vestibulum quam mollis puruseleifend, nec condimentum massa fermentum.
+            Phasellus at dui accumsan, volutpat magna ac, blandit ipsum."""
+    )
     String bio,
+
+    @Schema(description = "Profile type", example = "USER")
     @NotNull(message = "Profile type is mandatory")
     @Enumerated(EnumType.STRING)
     ProfileTypeEnum profileType
