b622456010d627a000752dd315102e3f30e50cd2
hywenklis <hywenklis@hotmail.com>
2025-07-22 15:31:13 -0300

    adiciona authorities

--- src/main/java/com/buddy/api/domains/authentication/dtos/AuthenticatedUser.java
+++ src/main/java/com/buddy/api/domains/authentication/dtos/AuthenticatedUser.java
@@ -53,7 +53,7 @@ public class AuthenticatedUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 }
 
