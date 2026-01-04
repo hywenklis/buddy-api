ab0c232f2cd3b264e5cc6da6f0f3c32907ba80c7
hywenklis <hywenklis@hotmail.com>
2026-01-04 10:51:53 -0500

    fix: adjust integration test abstract

--- src/test/java/com/buddy/api/integrations/IntegrationTestAbstract.java
+++ src/test/java/com/buddy/api/integrations/IntegrationTestAbstract.java
@@ -17,6 +17,7 @@ import com.buddy.api.domains.terms.repositories.TermsVersionRepository;
 import com.buddy.api.integrations.configs.TestContainersConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
