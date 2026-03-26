d12944daa731889c71226c78001912192a4f1217
hywenklis <hywenklis@hotmail.com>
2026-03-26 11:31:23 -0500

    feat(security) ajusta checkstyle

--- src/main/java/com/buddy/api/commons/configurations/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/buddy/api/commons/configurations/security/jwt/JwtAuthenticationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
